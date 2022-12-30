049f0b712ddfa27f496eddb1f282f2f2bc613eea
Suphakrit Boonlar <112085586+Fakemilaz@users.noreply.github.com>
2022-12-30 22:59:27 +0700

    updated!

---
+++ src/components/Main.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+export default function Main() {
+    return (
+        <>
+            <div>
+                <h1 className="font-bold text-2xl mt-20 mx-5 text-center">Countdown Time of 2023 🗓</h1>
+            </div>
+        </>
+    )
+}
