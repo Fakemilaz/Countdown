b3dc155996c282fb7fcdf33bb77c680958960dc4
Suphakrit Boonlar <112085586+Fakemilaz@users.noreply.github.com>
2022-12-30 22:21:20 +0700

    updated!

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+export default function Navbar() {
+    return (
+        <>
+            <div className="md:container md:mx-auto shadow-lg py-5 px-5 rounded-lg flex justify-between">
+                <h1 className="font-bold text-xl">⏱ Let's Countdown</h1>
+
+                <button className="">Contact Me</button>
+            </div>
+        </>
+    )
+}
