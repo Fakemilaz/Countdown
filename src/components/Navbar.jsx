e009410b4ec525c2120f8557a3ae027b258306c0
Suphakrit Boonlar <112085586+Fakemilaz@users.noreply.github.com>
2022-12-30 22:47:30 +0700

    Update Navbar.jsx

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,10 +3,10 @@ import React from "react";
 export default function Navbar() {
     return (
         <>
-            <div className="md:container md:mx-auto shadow-lg py-5 px-5 rounded-lg flex justify-between">
+            <div className="md:container md:mx-auto shadow-lg py-5 px-5 rounded-lg flex justify-between items-center text-center">
                 <h1 className="font-bold text-xl">⏱ Let's Countdown</h1>
 
-                <button className="">Contact Me</button>
+                <button className="bg-gradient-to-r from-violet-500 to-fuchsia-500 text-white px-2 py-2 rounded-lg">Contact Me</button>
             </div>
         </>
     )
