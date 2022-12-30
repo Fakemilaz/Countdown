bf48604080a114467cbd8ea9d89e5b5ab4640075
Suphakrit Boonlar <112085586+Fakemilaz@users.noreply.github.com>
2022-12-31 02:15:05 +0700

    Update Navbar.jsx

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -4,7 +4,7 @@ export default function Navbar() {
     return (
         <>
             <div className="md:container md:mx-auto shadow-lg py-5 px-5 rounded-lg flex justify-between items-center text-center">
-                <h1 className="font-bold text-xl">⏱ Let's Countdown</h1>
+                <h1 className="font-bold text-xl select-none">⏱ Let's Countdown</h1>
                 <a href="https://www.facebook.com/suphakrit.boonlar"><button className="bg-gradient-to-r from-violet-500 to-fuchsia-500 text-white px-2 py-2 rounded-lg">Contact Me</button></a>
             </div>
         </>
