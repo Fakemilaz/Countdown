c7483c02372cce969d5832d8c800b5a379f0f06a
Suphakrit Boonlar <112085586+Fakemilaz@users.noreply.github.com>
2023-01-01 00:13:47 +0700

    updated

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -2,7 +2,7 @@ import React, { useEffect } from "react";
 
 export default function Main() {
     const Countdown = () => {
-        var countDownDate = new Date("Jan 1, 2023 00:00:00").getTime();
+        var countDownDate = new Date("Jan 1, 2024 00:00:00").getTime();
 
         var x = setInterval(function () {
 
@@ -32,7 +32,7 @@ export default function Main() {
     return (
         <>
             <div>
-                <h1 className="font-bold text-2xl mt-20 mx-5 text-center">Countdown Time of 2023 🗓</h1>
+                <h1 className="font-bold text-2xl mt-20 mx-5 text-center">Countdown Time of 2024 🗓</h1>
                 <p className="text-lg text-center" id="demo"></p>
                 <img className="w-56 mx-auto my-0 mt-20" src="https://camo.githubusercontent.com/b1f010b2737341e33371137cc7de1eb109bad190fbe12c2a7a6baa6a13e81ae5/68747470733a2f2f692e70696e696d672e636f6d2f6f726967696e616c732f39642f39622f64312f39643962643133616663653161373938643232656366643938393737333065642e676966" alt="computer" />
             </div>
